d9865ca1d0bf1caa1db1a28111c2bdd22fbce3fc
vyas2610 <deepakvyas261092@gmail.com>
2024-05-06 20:36:12 +0530

    CURD DOne

--- src/App.js
+++ src/App.js
@@ -7,40 +7,15 @@ function App() {
   //use state
   const [title, setTitle] = useState("");
   const [desc, setDesc] = useState("");
+  const [id, setId] = useState(null);
   async function fetchItems() {
-    // axios.get("/todos")
-    //   .then((res) => {
-    //     console.log(res);
-    //   }).catch(() => {
-    //     console.error("Error...!")
-    //   })
     try {
       let ApiResponse = await ApiExe("todos");
       updateItem(ApiResponse?.data?.response);
     } catch (error) {
-      // alert("Error occurs: " + JSON.stringify(error));
       console.warn(JSON.stringify(error))
     }
-    // setTimeout(() => {
-    //   updateItem([
-    //     {
-    //       titile: "Title | What to  do",
-    //       description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum`,
-    //     },
-    //     {
-    //       titile: "Title | What to  do",
-    //       description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum`,
-    //     },
-    //     {
-    //       titile: "Title | What to  do 1",
-    //       description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum`,
-    //     },
-    //     {
-    //       titile: "Title | What to  do",
-    //       description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum`,
-    //     },
-    //   ])
-    // }, 3000)
+
   }
   //Mount 
 
@@ -49,9 +24,55 @@ function App() {
   }, []);
 
 
+  const EditData = async (id) => {
+    //  alert(id) 
+    try {
+      let ApiResp = await ApiExe(`todos/${id}`);
+      setTitle(ApiResp.data.response.task_name);
+      setDesc(ApiResp.data.response.task_desc);
+      setId(id);
+    } catch (error) {
+      alert("Error occurs: " + JSON.stringify(error));
+    }
+  }
   const [items, updateItem] = useState([]);
-  const showData = () => {
-    alert(JSON.stringify({ title, desc }))
+  const DeleteData = async (id) => {
+    //alert(id)
+    try {
+      await ApiExe(`todos/${id}`, "DELETE");
+      fetchItems();
+    } catch (error) {
+      alert("Error occurs: " + JSON.stringify(error));
+    }
+  }
+  const showData = async (event) => {
+    event.preventDefault();
+    // alert(JSON.stringify({ title, desc }))
+
+
+    if (id) {
+      //update
+      await ApiExe(`todos/${id}`, "PUT", {
+        data: {
+          task_name: title,
+          task_desc: desc
+        },
+      })
+
+    }
+    else {
+      //add
+      await ApiExe("todos", "POST", {
+        data: {
+          task_name: title,
+          task_desc: desc
+        },
+      })
+    }
+    setTitle("");
+    setDesc("");
+    setId(null);
+    fetchItems();
   }
 
   return (
@@ -63,8 +84,8 @@ function App() {
           <div className="form-content">
             <div className="form-data">
               <input placeholder="Enter Your Task" value={title} onInput={(event) => setTitle(event.target.value)} />
-              <p><textarea placeholder="Enter Description" onInput={(e) => setDesc(e.target.value)}>{desc}</textarea></p>
-              <button>Save</button>
+              <p><textarea placeholder="Enter Description" onInput={(e) => setDesc(e.target.value)} value={desc}></textarea></p>
+              <button>{id ? "Update" : "Save"}</button>
             </div>
             <div>
             </div>
@@ -78,6 +99,7 @@ function App() {
             <div className="entry" key={index}>
               <h1>{data.task_name}</h1>
               <p>{data.task_desc}</p>
+              <button onClick={() => EditData(data.id)}>Edit</button> | <button onClick={() => DeleteData(data.id)}>Delete</button>
             </div>
           ))}
 
